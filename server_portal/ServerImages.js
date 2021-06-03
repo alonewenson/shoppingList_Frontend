ec59996821057492e0db4dbead155565ca620708
Alon Ewenson <alonewenson@gmail.com>
2021-06-03 15:04:27 +0300

    changed backend route

--- server_portal/ServerImages.js
+++ server_portal/ServerImages.js
@@ -5,7 +5,7 @@ const DEFAULT_IMG = 'https://cdn.pixabay.com/photo/2016/12/21/17/11/signe-192336
 export const getImg = async(name) => {
   let result = DEFAULT_IMG;
   try {
-    const axiosRes = await axios.get(`http://localhost:3000/getImg/`+ name);    //TODO move server address to env file
+    const axiosRes = await axios.get(`http://localhost:3000/getImg/getImgByName/`+ name);    //TODO move server address to env file
     result = axiosRes.data;
   } catch (error) {
     console.log('connection to server returned error: ');
@@ -18,7 +18,7 @@ export const getImg = async(name) => {
 export const getImgsGallery = async(name) => {
   let result = [DEFAULT_IMG];
   if(true){
-    const axiosRes = await axios.get(`http://localhost:3000/getImgsGallery/`+ name);  //TODO move server address to env file
+    const axiosRes = await axios.get(`http://localhost:3000/getImg/getImgsGallery/`+ name);  //TODO move server address to env file
     result = axiosRes.data;
   }
   return result;
